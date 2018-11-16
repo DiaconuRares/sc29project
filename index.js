917265af4a80c8c8fcb3f8836a777ef92e65cf3a
DiaconuRares <40871918+DiaconuRares@users.noreply.github.com>
2018-11-16 21:44:32 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $(".links").hide();
+    $(".cont").hide();
+    $(".container0").fadeIn(1000);
     
     $(".header>.link0").click(function() {
         $(".links1").fadeOut(500);
@@ -27,5 +29,15 @@ $(document).ready(function() {
         $(".links2").fadeOut(500);
         $(".links0").fadeOut(500);
         $(".links3").slideToggle(800);
+    });
+    
+    $(".links0>ul>.link0").click(function() {
+        $(".links").fadeOut(500);
+        $(".cont").fadeOut(500);
+        $(".container1").fadeIn(800);
+    });
+    
+    $(".container1>.text5>h2").click(function() {
+        window.location.href = "pdi16-20.pdf";
     });
 });
